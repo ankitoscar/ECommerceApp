031610fbcc14a9055cb4bc6ce887916a92377ef5
ankitoscar <ankitoscar911@gmail.com>
2022-03-12 15:50:43 +0530

    Added products and cart page

---
+++ entry/src/main/js/default/pages/products/products.js
@@ -0,0 +1,21 @@
+import router from '@system.router';
+export default {
+    data: {
+        title: 'World'
+    },
+    build: function(){
+        router.push({
+            uri: "pages/build/build",
+        });
+    },
+    index: function(){
+        router.push({
+            uri: "pages/index/index",
+        });
+    },
+    cart: function(){
+        router.push({
+            uri: "pages/cart/cart",
+        });
+    },
+}
